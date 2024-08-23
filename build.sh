cf4a9897a5f82e1f64b1ecc5ead6d9304b607992
AndreMendes21 <andre.f.mendes24@gmail.com>
2024-08-24 00:12:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@
 set -e
 SRC_DIR=$PWD
 
+# Compile main.c in EDKII
 source ../edksetup.sh
 build -a X64 -t GCC5 -p MdeModulePkg/MdeModulePkg.dsc
 cp ../Build/MdeModule/DEBUG_GCC5/X64/Pong.efi $SRC_DIR/BOOTX64.EFI
 
+# Create the UEFI image
 mkdir -p image/EFI/BOOT
 cp ./BOOTX64.EFI image/EFI/BOOT/
 
-qemu-system-x86_64 -net none -bios $SRC_DIR/OVMF_dir/OVMF.fd -drive file=fat:rw:image,media=disk,format=raw
+# Boot qemu
+qemu-system-x86_64 -net none -bios $SRC_DIR/OVMF.fd -drive file=fat:rw:image,media=disk,format=raw
